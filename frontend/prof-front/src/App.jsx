ec7ec672cee77f7874f8175869939c22e08370d0
MdRaselHosen <raselh280@gmail.com>
2026-03-10 22:44:36 +0600

    add homepage,navbar

--- frontend/prof-front/src/App.jsx
+++ frontend/prof-front/src/App.jsx
@@ -1,18 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// Components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
-// Pages
-import Home from './pages/Home';
-import PropertyDetails from './pages/PropertyDetails';
-import SearchResults from './pages/SearchResults';
-import AddProperty from './pages/AddProperty';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
@@ -22,12 +13,6 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/property/:id" element={<PropertyDetails />} />
-            <Route path="/search" element={<SearchResults />} />
-            <Route path="/add-property" element={<AddProperty />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </main>
         <Footer />
