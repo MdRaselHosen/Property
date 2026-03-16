97a7f55b17f64796f1bf92b89acccea169d2c705
MdRaselHosen <raselh280@gmail.com>
2026-03-16 13:07:59 +0600

    add models for properties

---
+++ backend/property/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from user.models import User
+
+# Create your models here.
+class Property(models.Model):
+    class PropertyType(models.TextChoices):
+        HOUSE = 'house', 'House'
+        APARTMENT = 'apartment', 'Apartment'
+        LAND = 'land', 'Land'
+
+    class Status(models.TextChoices):
+        SALE = 'sale', 'For Sale'
+        RENT = 'rent', 'For Rent'
+
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12,decimal_places=2, blank=False, null=False)
+    property_type = models.CharField(max_length=20, choices=PropertyType.choices)
+    status = models.CharField(max_length=20, choices=Status.choices)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Location(models.Model):
+    property = models.OneToOneField(Property, on_delete=models.CASCADE)
+    city = models.CharField(max_length=200)
+    area = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longtitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+class PropertyImages(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='property_images/')
+
+class Reviews(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
