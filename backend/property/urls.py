1bfe83b2517703bf733daa89a0372ad2483adc9b
MdRaselHosen <raselh280@gmail.com>
2026-03-25 16:23:07 +0600

    load data from backend

--- backend/property/urls.py
+++ backend/property/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PropertyViewSet
+
+router = DefaultRouter()
+router.register(r'property', PropertyViewSet)
 
 urlpatterns = [
-    
+    path('',include(router.urls))
 ]
--- backend/property/views.py
+++ backend/property/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import PropertySerializers
+from rest_framework.filters import SearchFilter
 # Create your views here.
 
-class PropertyView(APIView):
-    pass
+class PropertyViewSet(ModelViewSet):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializers
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description','price']
+
