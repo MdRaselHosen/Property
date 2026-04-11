e3a08193ff3f56a61eb29fc1c11a089f355be451
MdRaselHosen <raselh280@gmail.com>
2026-04-11 12:48:41 +0600

    add property reviews backend

--- backend/property/views.py
+++ backend/property/views.py
@@ -3,10 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from .models import *
-from .serializers import PropertySerializers
+from .serializers import PropertySerializers, ReviewSerializer
 from rest_framework.filters import SearchFilter
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly
 # Create your views here.
@@ -14,7 +15,7 @@ from .permissions import IsOwnerOrReadOnly
 class PropertyViewSet(ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializers
-    parser_classes = [MultiPartParser, FormParser]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
     filter_backends = [SearchFilter]
     search_fields = ['title', 'description','price','location__city']
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
@@ -22,3 +23,18 @@ class PropertyViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    @action(detail=True, methods=['get', 'post'], permission_classes=[IsAuthenticatedOrReadOnly])
+    def reviews(self, request, pk=None):
+        """Get all reviews for a property or create a new review"""
+        property = self.get_object()
+        
+        if request.method == 'POST':
+            serializer = ReviewSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(property=property, user=request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        reviews = property.reviews.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
